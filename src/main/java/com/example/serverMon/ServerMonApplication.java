6091140750f8919816353a3efeb7f5472d2775a0
FulcrumDv <jackwellis0@gmail.com>
2024-09-02 15:56:26 +0100

    first commit

---
+++ src/main/java/com/example/serverMon/ServerMonApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverMon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerMonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerMonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/serverMon/ServerMonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverMon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerMonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
